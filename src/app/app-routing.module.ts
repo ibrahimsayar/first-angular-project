89835a38ab226efd953734c9c47ac88b82646c4f
ibrahim.sayar <ibrahim.sayar@naylalabs.com>
2022-06-09 12:57:33 +0300

    Home page created.
    Wip.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
+import {WtfComponent} from "./components/wtf/wtf.component";
+import {ContactComponent} from "./components/contact/contact.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'wtf',
+    component: WtfComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/components/wtf/wtf.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WtfComponent } from './wtf.component';
+
+describe('WtfComponent', () => {
+  let component: WtfComponent;
+  let fixture: ComponentFixture<WtfComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WtfComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WtfComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
